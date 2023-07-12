782ab7dca4885a261096f37a7b894e0b7fd15482
iren <irasibil2@gmail.com>
2023-07-12 22:28:00 +0300

    implement jwt

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', [
+            'except' => [
+                'login',
+                'register'
+            ]
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        auth()->login($user);
+
+        return self::respondWithToken();
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Login Failed'
+            ]);
+        }
+
+        return self::respondWithToken();
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function refresh()
+    {
+        return self::respondWithToken();
+    }
+
+    public function user()
+    {
+        return response()->json(auth()->user());
+
+    }
+
+    public function respondWithToken()
+    {
+        return response()->json([
+            'access_token' => auth()->refresh(),
+            'type' => 'Bearer',
+            'expires_in' => \Config::get('jwt.ttl')
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('auth')
+    ->middleware('api')
+    ->controller(AuthController::class)
+    ->group(function() {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+
+
+        Route::get('user', [AuthController::class, 'user']);
+});
